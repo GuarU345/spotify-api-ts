95ed60d2ffd21f3d6b67f104b776a50cf3ff18a4
guaru345 <abelmtz34@outlook.com>
2023-10-17 11:28:08 -0600

    add endpoints to test user and songs data and add morgan to manage the response of endpoints

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import { router } from "./routes/routes";
 
 export const app = express();
 export const PORT = process.env.PORT ?? 3000;
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use("/", router);
