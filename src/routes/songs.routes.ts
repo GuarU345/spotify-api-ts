72b11b6e6f538f38840cf12af9463eb54b87c483
guaru345 <abelmtz34@outlook.com>
2023-10-19 10:26:32 -0600

    add endpoint to start use the api

--- src/routes/songs.routes.ts
+++ src/routes/songs.routes.ts
@@ -4,9 +4,9 @@ import { authenticate } from "../middlewares/authenticate";
 
 const router = Router();
 
-router.post("/api/album/:id/songs", SongController.createNewSong);
+router.post("/api/albums/:id/songs", SongController.createNewSong);
 router.get("/api/songs", SongController.getSongs);
-router.get("/api/album/:id/songs", SongController.getSongsByAlbum);
+router.get("/api/albums/:id/songs", SongController.getSongsByAlbum);
 router.get("/api/user/:id/songs", SongController.getLikedSongsByUser);
 
 export const songRoutes = router;
