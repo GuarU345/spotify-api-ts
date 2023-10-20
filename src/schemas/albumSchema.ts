d1b1bbb4476cecd94636f328e6c2bddc56b34d84
guaru345 <abelmtz34@outlook.com>
2023-10-20 13:51:09 -0600

    fix problem with image if you create a album

--- src/schemas/albumSchema.ts
+++ src/schemas/albumSchema.ts
@@ -17,7 +17,9 @@ export const albumSchema = z.object({
         message: "La fecha de lanzamiento no es válida en formato ISO 8601",
       }
     ),
-  album_image: z.string({
-    invalid_type_error: "La url del album debe de ser un string",
-  }),
+  album_image: z
+    .string({
+      invalid_type_error: "La url del album debe de ser un string",
+    })
+    .optional(),
 });
