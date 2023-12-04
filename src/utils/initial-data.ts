0eb8cfca17bbb16200056371a6ab1ec56cebb09a
guaru345 <abelmtz34@outlook.com>
2023-12-04 08:44:45 -0600

    remove initial data from the seed to a respective initial data file

---
+++ src/utils/initial-data.ts
@@ -0,0 +1,193 @@
+export const ARTISTS = [
+  {
+    name: "Imagine Dragons",
+    nationality: "American",
+  },
+  {
+    name: "Post Malone",
+    nationality: "American",
+  },
+  {
+    name: "The Weeknd",
+    nationality: "American",
+  },
+  {
+    name: "Kendrick Lamar",
+    nationality: "American",
+  },
+  {
+    name: "Junior H",
+    nationality: "Mexican",
+  },
+];
+
+export const ALBUMS = [
+  {
+    name: "Smoke + Mirrors",
+    release_date: "1970-01-01T00:00:00.000Z",
+    album_image:
+      "https://i.scdn.co/image/ab67616d00001e021551c93dfa33ea4f30ef4eea",
+    artist: "Imagine Dragons",
+  },
+  {
+    name: "Beerbongs & Bentleys",
+    release_date: "1970-01-01T00:00:00.000Z",
+    album_image:
+      "https://i.scdn.co/image/ab67616d00001e02b1c4b76e23414c9f20242268",
+    artist: "Post Malone",
+  },
+  {
+    name: "After Hours",
+    release_date: "1970-01-01T00:00:00.000Z",
+    album_image:
+      "https://i.scdn.co/image/ab67616d00001e028863bc11d2aa12b54f5aeb36",
+    artist: "The Weeknd",
+  },
+  {
+    name: "DAMN",
+    release_date: "1970-01-01T00:00:00.000Z",
+    album_image:
+      "https://i.scdn.co/image/ab67616d00001e028b52c6b9bc4e43d873869699",
+    artist: "Kendrick Lamar",
+  },
+  {
+    name: "$AD BOYZ 4 LIFE II",
+    release_date: "1970-01-01T00:00:00.000Z",
+    album_image:
+      "https://i.scdn.co/image/ab67616d00001e02f3ba6742a35f111aaea1ca2f",
+    artist: "Junior H",
+  },
+];
+
+export const SONGS = [
+  {
+    artist: "Imagine Dragons",
+    album: "Smoke + Mirrors",
+    songs: [
+      {
+        name: "Gold",
+        duration: "3:06",
+      },
+      {
+        name: "Shots",
+        duration: "4:05",
+      },
+      {
+        name: "Hopeless Opus",
+        duration: "5:25",
+      },
+      {
+        name: "Dream",
+        duration: "4:06",
+      },
+      {
+        name: "Who We Are",
+        duration: "3:30",
+      },
+    ],
+  },
+  {
+    artist: "Post Malone",
+    album: "Beerbongs & Bentleys",
+    songs: [
+      {
+        name: "Stay",
+        duration: "3:06",
+      },
+      {
+        name: "Over Now",
+        duration: "4:05",
+      },
+      {
+        name: "rockstar",
+        duration: "5:25",
+      },
+      {
+        name: "Psycho",
+        duration: "4:06",
+      },
+      {
+        name: "Better Now",
+        duration: "3:30",
+      },
+    ],
+  },
+  {
+    artist: "The Weeknd",
+    album: "After Hours",
+    songs: [
+      {
+        name: "In Your Eyes",
+        duration: "3:06",
+      },
+      {
+        name: "Save Your Tears",
+        duration: "4:05",
+      },
+      {
+        name: "Blinding Lights",
+        duration: "5:25",
+      },
+      {
+        name: "Faith",
+        duration: "4:06",
+      },
+      {
+        name: "Heartless",
+        duration: "3:30",
+      },
+    ],
+  },
+  {
+    artist: "Kendrick Lamar",
+    album: "DAMN",
+    songs: [
+      {
+        name: "BLOOD",
+        duration: "3:06",
+      },
+      {
+        name: "DNA",
+        duration: "4:05",
+      },
+      {
+        name: "ELEMENT",
+        duration: "5:25",
+      },
+      {
+        name: "FEEL",
+        duration: "4:06",
+      },
+      {
+        name: "DUCKWORTH",
+        duration: "3:30",
+      },
+    ],
+  },
+  {
+    artist: "Junior H",
+    album: "$AD BOYZ 4 LIFE II",
+    songs: [
+      {
+        name: "Y LLORO",
+        duration: "3:06",
+      },
+      {
+        name: "MIENTRAS DUERMES",
+        duration: "4:05",
+      },
+      {
+        name: "$AD BOYZ II",
+        duration: "5:25",
+      },
+      {
+        name: "ROCKSTAR",
+        duration: "4:06",
+      },
+      {
+        name: "SERPIENTE",
+        duration: "3:30",
+      },
+    ],
+  },
+];
